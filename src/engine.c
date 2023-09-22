26157f8567d3d49c202b651794ec3bd9e5f130e8
Kaspar Martin Suursalu <kasparsu@gmail.com>
2023-09-22 22:49:56 +0300

    Fix windows terminal output issue 3

--- src/engine.c
+++ src/engine.c
@@ -25,7 +25,7 @@ ENGINE_writeToLogFile(ENGINE* engine, const char* buffer) {
 internal void
 ENGINE_writeToLog(ENGINE* engine, const char* buffer) {
   // Output to console
-  sprintf("%s", buffer);
+  sprintf(buffer,"%s");
   ENGINE_writeToLogFile(engine, buffer);
 }
 
